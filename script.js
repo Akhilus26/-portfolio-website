6bd83bfe8bdfceab0c160b74b8b24b94785c5bc1
Akhil U S <akhilus321@gmail.com>
2023-11-23 00:50:44 +0530

    mobile resposive added

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-var tabLinks= document.getElementsByClassName("tab-links")
-var tabContents= document.getElementsByClassName("tab-contents")
+var tabLinks= document.getElementsByClassName("tab-links");
+var tabContents= document.getElementsByClassName("tab-contents");
 
 function opentab(tabName){
    for(tabLink of tabLinks){
-    tabLink.classList.remove("active-link")
+    tabLink.classList.remove("active-link");
    }
 
    for (tabContent of tabContents){
-    tabContent.classList.remove("active-tab")
+    tabContent.classList.remove("active-tab");
    }
 
-   event.currentTarget.classList.add("active-link")
+   event.currentTarget.classList.add("active-link");
 
-   document.getElementById(tabName).classList.add("active-tab")
+   document.getElementById(tabName).classList.add("active-tab");
+}
+
+//---------------------------------sidemenu----------------------------------------------//
+
+var sidemen = document.getElementById("sidemenu");
+
+function openmenu(){
+   sidemen.style.right = "0";
+
+}
+
+function closemenu(){
+   sidemen.style.right = "-200px";
 }
